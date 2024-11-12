1cb66299f409944b20ec5f184ff53e5927887567
sujin999 <sujin.herofactory@gmail.com>
2024-11-12 17:37:35 +0900

    update

---
+++ dist/components/row_rnd/interactable.d.ts
@@ -0,0 +1,11 @@
+import { DraggableOptions } from "@interactjs/actions/drag/plugin";
+import { ResizableOptions } from "@interactjs/actions/resize/plugin";
+import { Interactable } from "@interactjs/types";
+import { FC } from "react";
+export declare const InteractComp: FC<{
+    interactRef?: React.MutableRefObject<Interactable>;
+    draggable: boolean;
+    draggableOptions: DraggableOptions;
+    resizable: boolean;
+    resizableOptions: ResizableOptions;
+}>;
